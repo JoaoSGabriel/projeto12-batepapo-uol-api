d04d5065b8da10e6f7c30bd894fff67a0c522726
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-09-01 18:28:16 -0300

    apply functions from participantes route

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from "dotenv";
+import { MongoClient } from 'mongodb';
+dotenv.config();
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
-server.post('/participantes', (req, res) => {
-    const { Username } = req.body;
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
 
-    if (!Username) {
-        res.status(422).send({error: 'Todos os campos são obrigatórios!'});
-        return
-    }
-    // if (Username está em uso) {
-    //     res.status(409).send({error: 'Nome já está em uso'});
-    //     return;
-    // }
-
-    res.status(201);
+mongoClient.connect().then(() => {
+	db = mongoClient.db("batepapouol");
 });
 
-server.get('/participantes', (req, res) => {
+server.post('/participantes', (req,res) => {
+    const username = req.body.name;
 
+    if (!username) {
+        return res.status(422).send({error: 'Todos os campos são obrigatórios!'});
+    }
+
+    db.collection("participantes").findOne({
+        name: username
+    }).then(user => {
+        if (user === null) {
+            db.collection("participantes").insertOne({
+                name: username,
+                lastStatus: Date.now()
+            });
+            res.status(201).send("ok");
+            return;
+        } else {
+            return res.status(409).send({error: 'Este usuário já existe!'});
+        }
+    });
+})
+
+server.get('/participantes', (req, res) => {
+    db.collection("participantes").find().toArray().then(users => {
+		res.send(users);
+	});
 });
 
 server.post('/messages', (req, res) => {
-    const { to, text, type } = req.body;
-    const { from } = req.header.User;
-
-    if (!to || !text) {
-        res.status(422).send({error: 'Todos os campos são obrigatórios!'});
-        return;
-    } else if (type !== 'message' || type !== 'private_message') {
-        res.status(422);
-        return;
-    } 
-    // else if (participante não existente) {
-    //     res.status(422);
-    //     return;
-    // }
-
-    res.status(201);
-
+    
 });
 
 server.get('/messages', (req, res) => {
