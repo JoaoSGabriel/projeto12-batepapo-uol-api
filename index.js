902127d80b5f213cd583d10883a45dff68817c53
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-09-01 19:16:03 -0300

    apply functions for messages route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
+import dayjs from 'dayjs';
 import { MongoClient } from 'mongodb';
 dotenv.config();
 
@@ -46,11 +47,37 @@ server.get('/participantes', (req, res) => {
 });
 
 server.post('/messages', (req, res) => {
+    const { to, text, type } = req.body;
+    const from = req.headers.user;
+
+    if (!to || !text) {
+        res.status(422).send({error: 'Todos os campos são obrigatórios!'});
+        return;
+    }
     
+    db.collection("participantes").findOne({
+        name: from
+    }).then(user =>{
+        if (user === null) {
+            return res.status(422).send({error: 'Participante não existente'});
+        } else {
+            db.collection("messages").insertOne({
+                from: from,
+                to: to,
+                text: text,
+                type: type,
+                time: dayjs().format('HH:mm:ss')
+            });
+            res.status(201).send("ok");
+            return;
+        }
+    });
 });
 
 server.get('/messages', (req, res) => {
-
+    db.collection("messages").find().toArray().then(messages => {
+		res.send(messages);
+	});
 });
 
 server.post('/status', (req, res) => {
